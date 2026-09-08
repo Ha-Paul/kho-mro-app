beb4f0dd8f3ecc90675bdaeb280b84a9d1c20458
Ha-Paul <haducnam5005@gmail.com>
2026-09-08 16:05:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -177,11 +177,12 @@ df_dm, df_nhap, df_xuat = load_data()
 # ==========================================
 # 3. GIAO DIỆN CHÍNH (MAIN TABS)
 # ==========================================
-st.title("🏭 HỆ THỐNG QUẢN LÝ SẢN XUẤT & LOGISTICS")
+#st.title("🏭 HỆ THỐNG QUẢN LÝ SẢN XUẤT & LOGISTICS")
+st.title("🏭 HỆ THỐNG QUẢN LÝ SẢN XUẤT")
 
 main_tab1, main_tab2 = st.tabs([
-    "📊 QUẢN LÝ SẢN XUẤT",
-    "⚙️ HỆ THỐNG QUẢN LÝ MRO - FORMING (Tiêu chuẩn TPS)",
+   # "📊 QUẢN LÝ SẢN XUẤT",
+    "⚙️ HỆ THỐNG QUẢN LÝ MRO - FORMING",
 ])
 
 # ------------------------------------------
